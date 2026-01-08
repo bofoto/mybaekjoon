ab825209adcca9136fe2d5ab1f9a2063eff29552
bofoto <cho03k@gmail.com>
2026-01-08 23:00:28 +0900

    solved 10951 with Java & python

---
+++ 10951/10951.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while(true){
+            try{
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken()); 
+            bw.write((A+B)+"\n");            
+            }
+            catch (Exception e){
+                break;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
