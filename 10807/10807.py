69331c3e905301d44e59f10a5c9a86c6259ed266
bofoto <cho03k@gmail.com>
2026-01-13 21:44:10 +0900

    10807 python

---
+++ 10807/10807.py
@@ -0,0 +1,15 @@
+# N = int(input())
+# arr = list(map(int, input().split()))
+# M = int(input())
+
+# sum = 0
+
+# for i in arr:
+#     if M == arr:
+#         sum += 1
+# print(sum)
+
+N = int(input())
+arr = list(map(int, input().split()))
+M = int(input())
+print(arr.count(M))
